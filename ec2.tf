b2abcb2b9ccea1fb166c11460cf6c1d0613699df
MUJAHID <116384473+mujahidshaik28@users.noreply.github.com>
2024-03-19 13:06:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "python5" {
   ami                         = "ami-0cf7b2f456cd5efd4"
   instance_type               = "t2.micro"
-  key_name                    = "python"
+  key_name                    = "p4-key"
   vpc_security_group_ids      = [aws_security_group.ec2-SG.id]
   subnet_id                   = aws_subnet.subnet-1.id
   associate_public_ip_address = true
